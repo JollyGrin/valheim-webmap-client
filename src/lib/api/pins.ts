b4de450c66c6e45dba02f04cf03eaa90b6a6be7d
JollyGrin <lol@dean.lol>
2025-05-22 01:17:23 +0200

    add post

--- src/lib/api/pins.ts
+++ src/lib/api/pins.ts
@@ -1,8 +1,29 @@
-import { createQuery } from '@tanstack/svelte-query';
+import { createMutation, createQuery } from '@tanstack/svelte-query';
 import { apiClient } from './apiClient';
 
 export async function getAllPins() {
   const result = await apiClient.get('/pins');
+  return result.data;
+}
+
+export async function postNewPin({
+  x,
+  z,
+  type,
+  label
+}: {
+  x: number;
+  z: number;
+  type: string;
+  label: string;
+}) {
+  const result = await apiClient.post('/pins', {
+    x,
+    z,
+    type,
+    label,
+    userId: '515e04f2-002c-45bb-8828-b69db1e6cf46' // Static userId
+  });
   return result.data;
 }
 
@@ -11,3 +32,10 @@ export const useAllPins = () =>
     queryKey: ['pins'],
     queryFn: getAllPins
   });
+
+export const useNewPin = () => {
+  createMutation({
+    mutationKey: ['new-pin'],
+    mutationFn: postNewPin
+  });
+};
