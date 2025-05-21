d1e57295f9f5f8343c164a7addeb0404ad39454a
JollyGrin <lol@dean.lol>
2025-05-22 01:10:24 +0200

    change to prod url

--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const DEV_URL = 'http://localhost:3000';
+const PROD_URL = 'https://valheim-webmap-client-production.up.railway.app';
+
 export const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: PROD_URL,
   timeout: 2000
 });
