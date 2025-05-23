931456880b6954ddca5f2a72bdc45a54c2e2f237
JollyGrin <lol@dean.lol>
2025-05-23 17:20:18 +0200

    add dev

--- src/lib/api/apiClient.ts
+++ src/lib/api/apiClient.ts
@@ -1,9 +1,10 @@
+import { dev } from '$app/environment';
 import axios from 'axios';
 
 const DEV_URL = 'http://localhost:3000';
 const PROD_URL = 'https://valheim-webmap-client-production-645a.up.railway.app';
 
 export const apiClient = axios.create({
-  baseURL: PROD_URL,
+  baseURL: dev ? DEV_URL : PROD_URL,
   timeout: 2000
 });
