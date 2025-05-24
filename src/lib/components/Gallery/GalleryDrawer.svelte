52389652ad97b26ef899cc080b6874cbf4d3d79f
JollyGrin <lol@dean.lol>
2025-05-24 18:14:11 +0200

    add icon

--- src/lib/components/Gallery/GalleryDrawer.svelte
+++ src/lib/components/Gallery/GalleryDrawer.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import type { MediaGetDTO } from '$lib/api/media';
 	import { useAllPhotos } from '$lib/api/media';
+	import IconMedia from '$lib/icon/IconMedia.svelte';
 	import PinThumbnail from './PinThumbnail.svelte';
 
 	// State to track if drawer is expanded
-	let expanded = $state(true);
+	let expanded = $state(false);
 
 	// Function to toggle drawer state
 	function toggleDrawer() {
@@ -21,13 +22,21 @@
 </script>
 
 <div
-	class="absolute top-0 right-0 z-20 flex h-full flex-col gap-2 overflow-hidden border-l-[1px] border-sky-300/70 backdrop-blur-sm hover:border-sky-300/100 {expanded
-		? 'w-[275px] pt-1 pl-2'
+	class="absolute top-0 right-0 z-20 flex h-full flex-col gap-2 border-l-[1px] border-sky-300/70 backdrop-blur-sm hover:border-sky-300/100 {expanded
+		? 'w-[275px] overflow-hidden pt-1 pl-2'
 		: 'w-[5px]'}"
 	style="transition: width 300ms cubic-bezier(0.33, 1, 0.68, 1), padding 300ms cubic-bezier(0.33, 1, 0.68, 1);"
 	role="region"
 	aria-label="Gallery drawer"
 >
+	{#if !expanded}
+		<button
+			onclick={toggleDrawer}
+			class="absolute top-[50%] left-[-1.8rem] z-50 grid h-7 w-7 cursor-pointer place-items-center rounded-l-full bg-sky-300/70 p-1 text-white transition-all hover:bg-gradient-to-l hover:from-sky-300 hover:to-sky-300/70"
+		>
+			<IconMedia />
+		</button>
+	{/if}
 	{#if expanded}
 		{#each $query.data?.sort(sortLatestDateFirst) ?? [] as photo, i (i)}
 			<PinThumbnail data={photo} />
